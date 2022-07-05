29fdb6dfa13ccd1e2660c8a4ebe7c72080a2b93c
GT <66834354+GTDT@users.noreply.github.com>
2022-07-05 19:49:41 +0300

    Update Log4p.py

--- core/Log4p.py
+++ core/Log4p.py
@@ -6,6 +6,10 @@ class Logger:
         date_time = datetime.fromtimestamp(time.time())
         print(f"[{name} {date_time.strftime('%H:%M:%S')} {Fore.GREEN}INFO{Fore.RESET}] {msg}")
 
+    def content(self,name,msg,content):
+        date_time = datetime.fromtimestamp(time.time())
+        print(f"[{name} {date_time.strftime('%H:%M:%S')} {Fore.CYAN}CONTENT{Fore.RESET}] {msg} {Fore.MAGENTA}{content}{Fore.RESET}")
+
     def warn(self,name,msg):
         date_time = datetime.fromtimestamp(time.time())
         print(f"[{name} {date_time.strftime('%H:%M:%S')} {Fore.YELLOW}WARNING{Fore.RESET}] {msg}")
